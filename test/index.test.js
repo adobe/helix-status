0baa01d1ff5df4ad04ccea30af6a2bb2f49df376
Tobias Bocanegra <tripod@adobe.com>
2020-12-15 16:29:59 +0900

    chore: debug tests

--- test/index.test.js
+++ test/index.test.js
@@ -420,7 +420,6 @@ describe('Timeout Tests', () => {
       snail: url,
     }, {}, 10);
 
-    console.log(result);
     assert.ok(result.body.response_time >= 10);
     delete result.body.response_time;
 
